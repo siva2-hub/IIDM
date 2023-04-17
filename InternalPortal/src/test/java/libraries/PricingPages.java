9c84d1ba61a92466841184bc7b4b82f2a5eaa073
enterpi <enterpi@enterpi-H110M-H>
2023-04-17 18:31:30 +0530

    Fourth Commit was Added Discount codes are changed

--- InternalPortal/src/test/java/libraries/PricingPages.java
+++ InternalPortal/src/test/java/libraries/PricingPages.java
@@ -1,6 +1,7 @@
 package libraries;
 
 import java.time.Duration;
+import java.time.LocalTime;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -49,15 +50,16 @@ public class PricingPages extends App
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@class='ag-center-cols-container']")));
 		return stockCode;
 	}
-	public boolean verifyAddProduct(String stockCode, String discountCode, String listPrice) throws Exception
+	public boolean verifyAddProduct(String discountCode, String listPrice) throws Exception
 	{
 		boolean res = false;
+		String stockCode = this.getTime();
 		String expStockCode = this.addProduct(stockCode, discountCode, listPrice);
 		Thread.sleep(4000);
 		driver.findElement(By.xpath("//*[@class='css-wxvfrp']")).sendKeys(stockCode);
 		Thread.sleep(4000);
 		List<WebElement> txts = driver.findElement(By.xpath("//*[@class='ag-center-cols-container']")).findElements(By.xpath("//*[@row-index='0']"));
-//		System.out.println("comp name is "+txts.size());
+		//		System.out.println("comp name is "+txts.size());
 		List<WebElement> ls = txts.get(1).findElements(By.xpath("//*[contains(@class,'ag-cell ag-cell')]"));
 		String actStockCode = ls.get(0).getText();
 		if (expStockCode.equalsIgnoreCase(actStockCode)) {
@@ -86,10 +88,10 @@ public class PricingPages extends App
 		driver.findElement(By.id("react-select-4-input")).sendKeys(Keys.ENTER);
 		driver.findElement(By.id("react-select-5-input")).sendKeys("11");
 		qp.selectDropDown("11");
-		driver.findElement(By.id("react-select-6-input")).sendKeys("0.25");
-		driver.findElement(By.id("react-select-7-input")).sendKeys("0.50");
-		driver.findElement(By.id("react-select-8-input")).sendKeys("0.75");
-		driver.findElement(By.id("react-select-9-input")).sendKeys("0.80");
+		driver.findElement(By.xpath("//*[@placeholder='Our Price']")).sendKeys("0.25");
+		driver.findElement(By.xpath("//*[@placeholder='MRO']")).sendKeys("0.50");
+		driver.findElement(By.xpath("//*[@placeholder='OEM']")).sendKeys("0.75");
+		driver.findElement(By.xpath("//*[@placeholder='RS']")).sendKeys("0.80");
 	}
 	public void addButton(String btnName) {
 		List<WebElement> btns = driver.findElement(By.tagName("section")).findElements(By.tagName("button"));
@@ -114,5 +116,13 @@ public class PricingPages extends App
 			}
 		}
 	}
-	
+	public String getTime() {
+		LocalTime time = LocalTime.now();
+		System.out.println(String.valueOf(time));
+		String t1 =String.valueOf(time).replace(":", "");
+		String t2 = t1.replace(".", "");
+		
+		System.out.println("Current time: "+t2);
+		return t2;
+	}
 }
--- InternalPortal/src/test/java/testcases/PricingTestCases.java
+++ InternalPortal/src/test/java/testcases/PricingTestCases.java
@@ -10,18 +10,19 @@ public class PricingTestCases extends App
 {
 	PricingPages price = new PricingPages();
 	//@Test(enabled = false)
-	@Test(priority = 1)
-	public void testCase1() throws Exception {
-		boolean res = price.verifyAddProduct("newsc_1236", "BACO55", "120.12");
-		Assert.assertTrue(res);
-		App.logout();
-	}
-	//@Test(enabled = false)
-//	@Test(priority = 2)
-//	public void testCase2() throws Exception {
-//		App.login();
-//		price.addDiscountCode();
-//		//			Assert.assertTrue(res);
+//	@Test(priority = 1)
+//	public void testCase1() throws Exception {
+//		boolean res = price.verifyAddProduct( "BACO55", "120.12");
+//		Assert.assertTrue(res);
 //		App.logout();
 //	}
+	//@Test(enabled = false)
+	@Test(priority = 2)
+	public void testCase2() throws Exception {
+		App.login();
+		price.addDiscountCode();
+		//			Assert.assertTrue(res);
+		App.logout();
+	}
+	
 }
