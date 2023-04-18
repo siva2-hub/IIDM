5183b60b5005b0284670b451876f95b5265dcd4e
enterpi <enterpi@enterpi-H110M-H>
2023-04-18 17:02:35 +0530

    Update Product,Discount Code TestCases are Added

--- InternalPortal/src/test/java/libraries/PricingPages.java
+++ InternalPortal/src/test/java/libraries/PricingPages.java
@@ -28,6 +28,8 @@ public class PricingPages extends App
 			{
 				Thread.sleep(2000);
 				pricingMenu.get(i).click();
+				Thread.sleep(1500);
+				
 				break;
 			}
 		}
@@ -73,14 +75,52 @@ public class PricingPages extends App
 		}
 		return res;
 	}
-	public void addDiscountCode() throws Exception 
+	public String updateProduct() throws Exception 
+	{
+		this.pricingPage("Pricing");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(@class,'edit-icon-cell')]")));
+		Thread.sleep(1300);
+		driver.findElement(By.xpath("//*[contains(@class,'edit-icon-cell')]")).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("list_price")));
+		String text = driver.findElement(By.name("list_price")).getAttribute("value");
+		for(int i=0;i<text.length();i++) 
+		{
+			driver.findElement(By.name("list_price")).sendKeys(Keys.BACK_SPACE);
+		}
+		driver.findElement(By.name("list_price")).sendKeys("197");
+		Thread.sleep(1300);
+		this.addButton("Update Product");
+		return text;
+	}
+	public boolean verifyUpdateProduct() throws Exception 
+	{
+		boolean res = false;
+		String beforeEdit = this.updateProduct();
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("//*[contains(@class,'edit-icon-cell')]")).click();
+		Thread.sleep(2500);
+		String afterEdit = driver.findElement(By.name("list_price")).getAttribute("value");
+		this.closeIcon();
+		if (!beforeEdit.equalsIgnoreCase(afterEdit)) {
+			res = true;
+			Object status[] = {"PRICING_002_VerifyUpdateProduct", "before update List Price is "+beforeEdit, "after update List Price is "+afterEdit, "PricingPage", "Passed", java.time.LocalDate.now().toString()};
+			qp.values(status);
+		} else {
+			res = false;
+			Object status[] = {"PRICING_002_VerifyUpdateProduct", "before update List Price is "+beforeEdit, "after update List Price is "+afterEdit, "PricingPage", "Failed", java.time.LocalDate.now().toString()};
+			qp.values(status);
+		}
+		return res;
+	}
+	public String addDiscountCode() throws Exception 
 	{
 		this.pricingPage("Discount Codes");
 		Thread.sleep(2000);
+		String dc = "DC"+this.getTime();
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@class='Button-Icon-Display']")));
 		this.addImportExporBtns("Add");
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@name='discount_code']")));
-		driver.findElement(By.name("discount_code")).sendKeys("newdc1234");
+		driver.findElement(By.name("discount_code")).sendKeys(dc);
 		driver.findElement(By.id("react-select-3-input")).sendKeys("4");
 		driver.findElement(By.id("react-select-3-input")).sendKeys(Keys.ENTER);
 		driver.findElement(By.id("react-select-4-input")).sendKeys("8");
@@ -91,29 +131,92 @@ public class PricingPages extends App
 		driver.findElement(By.xpath("//*[@placeholder='MRO']")).sendKeys("0.50");
 		driver.findElement(By.xpath("//*[@placeholder='OEM']")).sendKeys("0.75");
 		driver.findElement(By.xpath("//*[@placeholder='RS']")).sendKeys("0.80");
-	}
-	public void verifyAddDiscountCode() throws Exception 
-	{
-		String dc = this.addDiscountCode();
-		Thread.sleep(1000);
+		Thread.sleep(1500);
 		this.addButton("Add Discount Code");
-		Thread.sleep(4000);
-		driver.findElement(By.xpath("//*[@class='css-wxvfrp']")).sendKeys(stockCode);
+		Thread.sleep(7000);
+		return dc;
+	}
+	
+	public boolean verifyAddDiscountCode() throws Exception 
+	{
+		String dc = this.addDiscountCode();boolean res = false;
+		System.out.println("dc was added.......!");
+		driver.findElement(By.xpath("//*[@class='quote-search-width']")).findElement(By.xpath("//*[@class='css-wxvfrp']")).sendKeys(dc);
 		Thread.sleep(4000);
 		List<WebElement> txts = driver.findElement(By.xpath("//*[@class='ag-center-cols-container']")).findElements(By.xpath("//*[@row-index='0']"));
 		//		System.out.println("comp name is "+txts.size());
 		List<WebElement> ls = txts.get(1).findElements(By.xpath("//*[contains(@class,'ag-cell ag-cell')]"));
 		String actStockCode = ls.get(0).getText();
-		if (expStockCode.equalsIgnoreCase(actStockCode)) {
+		if (dc.equalsIgnoreCase(actStockCode)) {
 			res = true;
-			Object status[] = {"PRICING_001_VerifyAddProduct", actStockCode, expStockCode, "PricingPage", "Passed", java.time.LocalDate.now().toString()};
+			Object status[] = {"PRICING_003_VerifyAddDiscountCode", actStockCode, dc, "PricingPage", "Passed", java.time.LocalDate.now().toString()};
 			qp.values(status);
 		} else {
 			res = false;
-			Object status[] = {"PRICING_001_VerifyAddProduct", actStockCode, expStockCode, "PricingPage", "Failed", java.time.LocalDate.now().toString()};
+			Object status[] = {"PRICING_003_VerifyAddDiscountCode", actStockCode, dc, "PricingPage", "Failed", java.time.LocalDate.now().toString()};
 			qp.values(status);
 		}
 		return res;
+	}
+	public String updateDiscountCode() throws Exception 
+	{
+		this.pricingPage("Discount Codes");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(@class,'edit-icon-cell')]")));
+		Thread.sleep(1300);
+		driver.findElement(By.xpath("//*[contains(@class,'edit-icon-cell')]")).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@placeholder='Our Price']")));
+		WebElement ourPrice = driver.findElement(By.xpath("//*[@placeholder='Our Price']"));
+		String text = ourPrice.getAttribute("value");
+		for(int i=0;i<text.length();i++) 
+		{
+			ourPrice.sendKeys(Keys.BACK_SPACE);
+		}
+		ourPrice.sendKeys("0.88");
+		Thread.sleep(1400);
+		this.addButton("Update Discount Code");
+		return text;
+	}
+	public boolean verifyUpdateDiscountCode() throws Exception 
+	{
+		String beforeUpdate = this.updateDiscountCode();boolean res = false;
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("//*[contains(@class,'edit-icon-cell')]")).click();
+		Thread.sleep(2500);
+		String afterUpdate = driver.findElement(By.xpath("//*[@placeholder='Our Price']")).getAttribute("value");
+		this.closeIcon();
+		if (!beforeUpdate.equalsIgnoreCase(afterUpdate)) {
+			res = true;
+			Object status[] = {"PRICING_004_VerifyUpdateDiscountCode", "before update Our Price value is "+beforeUpdate,
+					"after update Our Price value is "+afterUpdate, "PricingPage", "Passed", java.time.LocalDate.now().toString()};
+			qp.values(status);
+		} else {
+			res = false;
+			Object status[] = {"PRICING_004_VerifyUpdateDiscountCode", "before update Our Price value is "+beforeUpdate, 
+					"after update Our Price value is "+afterUpdate, "PricingPage", "Failed", java.time.LocalDate.now().toString()};
+			qp.values(status);
+		}
+		return res;
+	}
+	public void importFile() throws Exception 
+	{
+		String dcFile = "/home/enterpi/Downloads/apr2023PricingFile/BACO DISCOUNTCODEImport 2022.csv";
+		String pricingFile = "/home/enterpi/Downloads/apr2023PricingFile/bACO PriceList2023UPload.csv";
+		this.pricingPage("Pricing");
+		this.addImportExporBtns("Import");
+		Thread.sleep(2300);
+		List<WebElement> files = driver.findElements(By.xpath("//*[contains(@class,'file-import-box')]"));
+		System.out.println("count files type is "+files.size());
+		files.get(0).findElement(By.name("checkbox")).click();
+		Thread.sleep(2000);
+		files.get(0).findElement(By.xpath("//*[@type='file']")).sendKeys(dcFile);
+		Thread.sleep(2000);
+		files.get(1).findElement(By.name("checkbox")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div/div/section/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div/div"))
+			  .findElement(By.xpath("//*[@type='file']")).sendKeys(dcFile);
+//		files.get(1).findElement(By.xpath("//*[@type='file']")).sendKeys(dcFile);
+		Thread.sleep(5000);
+		this.addButton("Import");
 	}
 	public void addButton(String btnName) {
 		List<WebElement> btns = driver.findElement(By.tagName("section")).findElements(By.tagName("button"));
@@ -146,5 +249,9 @@ public class PricingPages extends App
 		
 		System.out.println("Current time: "+t2);
 		return t2;
+	}
+	public void closeIcon() 
+	{
+		driver.findElement(By.xpath("//*[@title='close']")).click();
 	}
 }
--- InternalPortal/src/test/java/testcases/PricingTestCases.java
+++ InternalPortal/src/test/java/testcases/PricingTestCases.java
@@ -1,5 +1,6 @@
 package testcases;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import commonUtils.App;
@@ -8,20 +9,44 @@ import libraries.PricingPages;
 public class PricingTestCases extends App
 {
 	PricingPages price = new PricingPages();
-	//@Test(enabled = false)
+	@Test(enabled = false)
 //	@Test(priority = 1)
-//	public void testCase1() throws Exception {
-//		boolean res = price.verifyAddProduct( "BACO55", "120.12");
+	public void testCase1() throws Exception {
+		boolean res = price.verifyAddProduct( "BACO55", "120.12");
+		Assert.assertTrue(res);
+		App.logout();
+	}
+	@Test(enabled = false)
+//	@Test(priority = 2)
+	public void testCase2() throws Exception {
+		App.login();
+		boolean res = price.verifyAddDiscountCode();
+		Assert.assertTrue(res);
+		App.logout();
+	}
+	@Test(enabled = false)
+//	@Test(priority = 3)
+	public void testCase3() throws Exception {
+		App.login();
+		boolean res = price.verifyUpdateProduct();
+		Assert.assertTrue(res);
+		App.logout();
+	}
+	@Test(enabled = false)
+//	@Test(priority = 4)
+	public void testCase4() throws Exception {
+		App.login();
+		boolean res = price.verifyUpdateDiscountCode();
+		Assert.assertTrue(res);
+		App.logout();
+	}
+//	@Test(enabled = false)
+	@Test(priority = 5)
+	public void testCase5() throws Exception {
+//		App.login();
+		price.importFile();
 //		Assert.assertTrue(res);
 //		App.logout();
-//	}
-	//@Test(enabled = false)
-	@Test(priority = 2)
-	public void testCase2() throws Exception {
-//		App.login();
-		price.addDiscountCode();
-		//			Assert.assertTrue(res);
-		App.logout();
 	}
 	
 }
